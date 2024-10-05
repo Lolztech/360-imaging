c8a4541933b789d346a562151b7d3d8938075199
Lolztech <123384950+Lolztech@users.noreply.github.com>
2024-10-05 16:25:32 +0100

    First Commit of initial work

---
+++ src/Main Files/NavModule.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NavModule.css';
+
+export default function NavModule() {
+  return (
+    <div>
+      <h2>Navigation</h2>
+      <p>This is the sidebar content.</p>
+    </div>
+  );
+}
---
+++ src/Main Files/ViewModule.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ViewModule.css';
+
+export default function ViewModule() {
+  return (
+    <div>
+      <h2>View Module</h2>
+      <p>This is where the 360-degree image will be displayed.</p>
+    </div>
+  );
+}
